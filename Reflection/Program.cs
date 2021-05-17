2a6cf6cf7fc9409a72aa2bbe83051fffd438a862
Arshad <arshadpinjari666@gmail.com>
2021-05-17 11:20:14 +0530

    Add project files.

---
+++ Reflection/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to reflection Demo");
+            ReflectionOnCustomer.ReflectionTest();
+            Console.ReadKey();
+        }
+    }
+}
